d528a50168d3332e8b670bf6acba081443842e27
sribalajii <isribalaji335@gmail.com>
2026-02-07 14:12:34 +0530

    feat:python code

---
+++ QSPIDER PYTHON FILE.py
@@ -0,0 +1,40 @@
+# 1. add two numbers
+# a=int(input("enter the number : "))
+# b=int(input("enter the number : "))
+# print(a+b)
+# print(f"the total value is {a+b}")
+
+# 2. WAPTF the square of the number:
+# c=int(input("enter the number : "))
+# print(f"the square of the {c} is: {c**2}")
+
+# 3. WAPT change float to integer number:
+# d=float(input("enter a number: "))
+# e=int(d)
+# print(e,type(e))
+
+# 4. WAPT print the last char form string:
+# f=input(" enter a name: ")
+# print(f" the last char of {f} is : {f[-1]}")
+
+# 5. WAPT reverse the string:
+# g=input("enter name: ")
+# print(f"the reverse of '{g}' is : {g[::-1]}")
+
+# 6. WAPT fetch middle value from list:
+# h=eval(input("enter value: "))
+# print([len(h)//2])
+
+# 7. WAPT  extarct last number from value:
+# i=(input("enter number: "))
+# print(f"the last value of {i} is  : {i[-1]}")
+q={'a':10,'b':20}
+print(q['a'],q.keys(),q.values())
+q['c']=30
+print(q)
+
+
+
+
+
+
