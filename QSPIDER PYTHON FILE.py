be6159872c83d1732e1cabc70fe2c52bd3509c1f
sribalajii <isribalaji335@gmail.com>
2026-02-09 22:57:05 +0530

    feat:python code

--- QSPIDER PYTHON FILE.py
+++ QSPIDER PYTHON FILE.py
@@ -21,6 +21,7 @@
 # g=input("enter name: ")
 # print(f"the reverse of '{g}' is : {g[::-1]}")
 
+
 # 6. WAPT fetch middle value from list:
 # h=eval(input("enter value: "))
 # print([len(h)//2])
@@ -29,8 +30,30 @@
 # i=(input("enter number: "))
 # print(f"the last value of {i} is  : {i[-1]}")
 
+# 8. WAPT simple if program
+# j=int(input("enter a number : "))
+# if j%2==0:
+#     print(f"{j} is even number ")
 
+# 9.WAPT CHECK THE given string is exactly 5 char or not?:
+# k=input("enter the  string: ")
+# if len(k)==5:
+#     print(f"'{k}' is a five char")
 
+# 10. WAP to check the given number is greater than 200 or not?
+# m=int(input("enter the number: "))
+# if m>200:
+#     print(f"'{m}' is greater then 200.")
 
+# 11. WAP to print the square of the number , if it's the multiple of 3 only:
+# n=int(input("enter the  number: "))
+# if n%3==0:
+#     print(f" the square of '{n}' is  {n**2}")
 
+#12. check the entered char is lower case or not?
+o = input("enter a char: ")
 
+if 'a' <= o <= 'z':
+    print(f"{o} is lower case")
+else:
+    print(f"{o} is not lower case")
