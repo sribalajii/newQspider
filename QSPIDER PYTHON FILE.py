825f2873a34c3bd4c30921a6ed47baf8c9f15557
sribalajii <isribalaji335@gmail.com>
2026-02-24 15:52:17 +0530

    feat:python code for LOOPING STATEMENT: #  13 wapt following output

--- QSPIDER PYTHON FILE.py
+++ QSPIDER PYTHON FILE.py
@@ -688,14 +688,21 @@ from os.path import split
 #     print(chr(i))
 
 # 12. wapt following output:
-n="hii baby how are you"
-out={}
-for i in (n.split()):
-        out[i]=len(i)
+# n="hii baby how are you"
+# out={}
+# for i in (n.split()):
+#         out[i]=len(i)
+# print(out)
+# print(n.split())# split() give sprate vau in string, its give value as list only.
+#
+
+
+# 13 wapt following output
+l=['python.py','google.com']
+out=[]
+for i in l:
+    out.append(i.split(".")[-1])
 print(out)
-print(n.split())# split() give sprate vau in string, its give value as list only.
-
-
 
 
 
