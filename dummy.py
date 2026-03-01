26ed51311f3e0b8edf0adc9f6e821b0d22abcb2e
sribalajii <isribalaji335@gmail.com>
2026-03-01 12:24:32 +0530

    feat:python code for LOOPING STATEMENT#

--- dummy.py
+++ dummy.py
@@ -1,7 +1,12 @@
 
-n=input("enter a name: ")
-print(f"the reverse of '{n}' is : '{n[::-1]}'")
-if n==n[::-1]:
-    print(f"it's a paladinrome {n}")
-else:
-    print(f"it's not paladinrome {n}")
+# n=input("enter a name: ")
+# print(f"the reverse of '{n}' is : '{n[::-1]}'")
+# if n==n[::-1]:
+#     print(f"it's a paladinrome {n}")
+# else:
+#     print(f"it's not paladinrome {n}")
+x = 5
+if x > 2:
+    print("A")
+if x > 4:
+    print("B")
