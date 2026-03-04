93f120678186b4139c09d61f44bfc8497637d334
sribalajii <isribalaji335@gmail.com>
2026-03-04 15:22:03 +0530

    feat:python code for LOOPING STATEMENT# 8. wapt following pattern. # # # # # #       # #       # #       # # # # # #

--- QSPIDER PYTHON FILE.py
+++ QSPIDER PYTHON FILE.py
@@ -1072,3 +1072,12 @@ for i in range(1,n+1):
         else:
             print("0",end=" ")
     print()
+# 8. wapt following pattern.
+n=5
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==1 or j==1 or i==5 or j==5:
+            print("#",end=" ")
+        else:
+            print("",end="  ")
+    print()
