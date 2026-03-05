8bea97ab107a7373b63fb9f68e060afb63836c7b
sribalajii <isribalaji335@gmail.com>
2026-03-05 15:42:39 +0530

    feat:python code for pattern# 16 1 1 2 1 2 3 1 2 3 4 1 2 3 4 5

--- QSPIDER PYTHON FILE.py
+++ QSPIDER PYTHON FILE.py
@@ -1238,15 +1238,15 @@
 #     print()
 
 # 16.
-1
-1 2
-1 2 3
-1 2 3 4
-1 2 3 4 5
-n=6
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i>j:
-           print(j,end=" ")
-    print()
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+# n=6
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i>j:
+#            print(j,end=" ")
+#     print()
 
