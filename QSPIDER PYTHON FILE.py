713fd3de7df0b417d2e55946dbe037d43cc957bb
sribalajii <isribalaji335@gmail.com>
2026-03-09 15:37:48 +0530

    feat:python code: pattern * * * * * * * * * * * * * * * * * * * * *

--- QSPIDER PYTHON FILE.py
+++ QSPIDER PYTHON FILE.py
@@ -1439,14 +1439,14 @@
 # 28.
 # * * * * * * *
 # *
-# * * * * * * *
+# * * * * *
 # *
 # * * * * * * *
 
 n=5
 for i in range(1,n+1):
     for j in range(1+n+1):
-        if (j==0 or i==1) or i==5 or i==3:
+        if (j==0 or i==1) or i==5 or (i==3 and j<n):
             print("*",end=" ")
         else:
             print(" ",end=" ")
